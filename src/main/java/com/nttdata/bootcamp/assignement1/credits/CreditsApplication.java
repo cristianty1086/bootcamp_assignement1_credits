5b6728ba0bf61e3809b2a3917e2a864c560ed4e7
cristianty1086 <ctinocoy123@gmail.com>
2022-10-24 15:30:43 -0500

    reconocible y con dockerfile

--- src/main/java/com/nttdata/bootcamp/assignement1/credits/CreditsApplication.java
+++ src/main/java/com/nttdata/bootcamp/assignement1/credits/CreditsApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.bootcamp.assignement1.credits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CreditsApplication {
 
 	public static void main(String[] args) {
