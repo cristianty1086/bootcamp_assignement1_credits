b840148e7fd41122ab9503f8f8715862e6e81bd3
cristianty1086 <ctinocoy123@gmail.com>
2022-11-02 13:20:27 -0500

    fix azure interconection of ws

--- src/main/java/com/nttdata/bootcamp/assignement1/credits/utilities/AppConstants.java
+++ src/main/java/com/nttdata/bootcamp/assignement1/credits/utilities/AppConstants.java
@@ -6,9 +6,9 @@ import java.util.Properties;
 
 public class AppConstants {
 
-    public static String baseUrl = "http://localhost";
-    public static String baseUrlCostumer = "http://localhost:8083";
-    public static String baseUrlCredits = "http://localhost:8085";
+    public static String baseUrl = "https://bootcamp37-credits.azurewebsites.net";
+    public static String baseUrlCostumer = "https://bootcamp37-costumers.azurewebsites.net";
+    public static String baseUrlCredits = "https://bootcamp37-credits.azurewebsites.net";
     public static String getCurrentUrl() {
         String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
         String appConfigPath = rootPath + "application.properties";
